2337216b098f429410e4d65798a33509fbaac8bf
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-05-26 14:38:18 +0100

    Removed orphaned experiment module (ciao-discharge-summary-parser) - code has been ported into main ciao-docs-parser repo

--- ciao-discharge-summary-parser/src/main/java/uk/nhs/ciao/docs/parser/PropertiesExtractor.java
+++
@@ -1,7 +0,0 @@
-package uk.nhs.ciao.docs.parser;
-
-import java.util.Map;
-
-public interface PropertiesExtractor<T> {
-	Map<String, Object> extractProperties(T document) throws UnsupportedDocumentTypeException;
-}
